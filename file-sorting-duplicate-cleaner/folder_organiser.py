330f2208d50169deaec28fe36f77029e9479114f
rjvision48-png <rjvision48@gmail.com>
2025-12-15 12:20:21 +0530

    Add folder organization by file extension
    Organizes files in a specified folder by their extensions into separate subfolders.

--- file-sorting-duplicate-cleaner/folder_organiser.py
+++ file-sorting-duplicate-cleaner/folder_organiser.py
@@ -1,1 +1,14 @@
-soon
+import os
+import shutil
+
+folder = input("Enter folder path: ")
+
+for file in os.listdir(folder):
+    path = os.path.join(folder, file)
+
+    if os.path.isfile(path):
+        ext = file.split(".")[-1]
+        dest = os.path.join(folder, ext)
+
+        os.makedirs(dest, exist_ok=True)
+        shutil.move(path, os.path.join(dest, file))
