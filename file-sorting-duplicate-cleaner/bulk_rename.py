b68bef57a9b2011e60fc17ab247c13250cecb191
rjvision48-png <rjvision48@gmail.com>
2025-12-15 12:20:54 +0530

    Implement bulk file renaming feature
    Added functionality to rename files in a specified folder sequentially.

--- file-sorting-duplicate-cleaner/bulk_rename.py
+++ file-sorting-duplicate-cleaner/bulk_rename.py
@@ -1,1 +1,12 @@
-later
+import os
+
+folder = input("Enter folder path: ")
+count = 1
+
+for file in os.listdir(folder):
+    path = os.path.join(folder, file)
+
+    if os.path.isfile(path):
+        new_name = f"file_{count}.{file.split('.')[-1]}"
+        os.rename(path, os.path.join(folder, new_name))
+        count += 1
