aad8a98ed88d2c6132b525dbe1f6256dbc51ef1b
rjvision48-png <rjvision48@gmail.com>
2025-12-15 12:24:49 +0530

    Update pdf_rename.py

--- pdf-split-merge/pdf_rename.py
+++ pdf-split-merge/pdf_rename.py
@@ -1,2 +1,12 @@
-# PDF Rename Tool - renames many PDF files based on your pattern.
-print("PDF Rename Tool - coming soon")
+import os
+
+folder = input("Enter folder path: ")
+count = 1
+
+for file in os.listdir(folder):
+    if file.endswith(".pdf"):
+        os.rename(
+            os.path.join(folder, file),
+            os.path.join(folder, f"pdf_{count}.pdf")
+        )
+        count += 1
