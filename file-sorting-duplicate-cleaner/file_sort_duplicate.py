7d13610c8836f4691db0bfb1ea34dea1cd811fea
rjvision48-png <rjvision48@gmail.com>
2025-12-15 12:19:49 +0530

    Update file_sort_duplicate.py

--- file-sorting-duplicate-cleaner/file_sort_duplicate.py
+++ file-sorting-duplicate-cleaner/file_sort_duplicate.py
@@ -1,49 +1,25 @@
 import os
-import shutil
 import hashlib
 
-FOLDER = "C:\Users\YourName\Downloads" #  CHANGE YOU FOLDER PATH HERE INSIDE "(WRITE YOUR FOLDER PATH HERE)" 
+folder = input("Enter folder path: ")
 
-# FILE SORTING PART 
-image_ext = [".jpg", ".jpeg", ".png"]
-doc_ext = [".pdf", ".docx"]
-
-images_folder = os.path.join(FOLDER, "Images")
-docs_folder = os.path.join(FOLDER, "Documents")
-
-os.makedirs(images_folder, exist_ok=True)
-os.makedirs(docs_folder, exist_ok=True)
-
-for f in os.listdir(FOLDER):
-    path = os.path.join(FOLDER, f)
-
-    if os.path.isfile(path):
-        if f.lower().endswith(tuple(image_ext)):
-            shutil.move(path, os.path.join(images_folder, f))
-
-        elif f.lower().endswith(tuple(doc_ext)):
-            shutil.move(path, os.path.join(docs_folder, f))
-
-print("File sorting done.")
-
-# DUPLICATE FINDER PART 
 seen = {}
 duplicates = []
 
-for f in os.listdir(FOLDER):
-    path = os.path.join(FOLDER, f)
+for file in os.listdir(folder):
+    path = os.path.join(folder, file)
 
-    if os.path.isfile(path):
-        with open(path, "rb") as file:
-            file_hash = hashlib.md5(file.read()).hexdigest()
+    if not os.path.isfile(path):
+        continue
 
-        if file_hash in seen:
-            duplicates.append(f)
-        else:
-            seen[file_hash] = f
+    with open(path, "rb") as f:
+        file_hash = hashlib.md5(f.read()).hexdigest()
 
-print("\nDuplicate files found:")
+    if file_hash in seen:
+        duplicates.append(file)
+    else:
+        seen[file_hash] = file
+
+print("Duplicate files:")
 for d in duplicates:
     print(d)
-
-print("\nProcess completed successfully.")
