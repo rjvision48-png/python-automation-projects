48a71fdc790d203c9168c388d89803834bf59eca
rjvision48-png <rjvision48@gmail.com>
2025-12-15 12:22:16 +0530

    Update column_merge.py

--- excel-automation/column_merge.py
+++ excel-automation/column_merge.py
@@ -1,2 +1,5 @@
-# Column Merge - combines multiple columns into one.
-print("Column Merge - coming soon")
+import pandas as pd
+
+df = pd.read_excel("data.xlsx")
+df["Merged"] = df["Column1"].astype(str) + " " + df["Column2"].astype(str)
+df.to_excel("merged.xlsx", index=False)
